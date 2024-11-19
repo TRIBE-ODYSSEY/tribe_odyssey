20e9278cc08b872d17c3d6595b9683b19fd6b166
dg-neural-23 <digitdose@proton.me>
2024-11-19 04:23:38 +0100

    Usunięto folder z repozytorium

--- tribe_odyssey/src/types/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import { Theme } from '@mui/material/styles';
-
-declare module '@mui/material/styles' {
-  interface CustomTheme extends Theme {
-    // Add any custom theme properties here
-  }
-  interface Theme extends CustomTheme {}
-  interface ThemeOptions extends CustomTheme {}
-} 
