58425bc1ec3b0481a7834bf1c4170ca8553b9c8e
Luciferhub44 <moltenwastelands@gmail.com>
2025-01-09 01:09:21 -0500

    Fix import path for index.css in main.tsx to ensure correct styling application

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import ErrorBoundary from '@src/components/common/errors/ErrorBoundary'
 import App from './App'
-import '../index.css'
+import './index.css'
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Failed to find the root element')
