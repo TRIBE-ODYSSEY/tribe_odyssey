a74596577824f4321f27c064aaad23dc922e163b
Luciferhub44 <moltenwastelands@gmail.com>
2025-01-10 11:25:37 -0500

    Refactor App component to integrate WagmiConfig and remove AuthProvider
    - Replaced AuthProvider with WagmiConfig for improved authentication management.
    - Updated the loading spinner fallback UI for better user experience.
    - Removed the useAxios hook file to streamline the codebase, as its functionality is no longer needed.
    - Enhanced overall structure and clarity of the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import { Spinner } from 'flowbite-react';
 import React, { Suspense } from 'react';
 import AppRoutes from './AppRoutes';
+import { WagmiConfig } from 'wagmi';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { AuthProvider } from './lib/contexts/AuthContext';
+import { config } from './wagmi';
+
 
 const App: React.FC = () => {
-  const auth = AuthProvider({ children: null });
-  
   return (
-    <auth.Provider value={auth.value}>
-      <Suspense
-        fallback={
-          <div className="">
-            <Spinner color="warning" aria-label="Info spinner example" />
-          </div>
-        }
-      >
-        <RainbowKitProvider>
-          <AppRoutes />
-        </RainbowKitProvider>
-      </Suspense>
-    </auth.Provider>
+    <WagmiConfig config={config}>
+        <Suspense
+          fallback={
+            <div className="flex h-screen items-center justify-center">
+              <Spinner color="warning" aria-label="Loading" />
+            </div>
+          }
+        >
+          <RainbowKitProvider>
+            <AppRoutes />
+          </RainbowKitProvider>
+        </Suspense>
+    </WagmiConfig>
   );
 };
 
