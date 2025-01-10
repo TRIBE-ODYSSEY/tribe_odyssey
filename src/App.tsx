8fcf29833fbbd899ca8dc4d47b08d288aa197129
Luciferhub44 <moltenwastelands@gmail.com>
2025-01-10 12:16:53 -0500

    Refactor App and main components to streamline provider usage and enhance structure
    - Removed QueryClientProvider from App.tsx, simplifying the component hierarchy.
    - Integrated WagmiProvider in main.tsx for improved context management.
    - Updated import statements for clarity and consistency.
    - Enhanced overall maintainability and readability of the application.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,18 @@
 import { Spinner } from 'flowbite-react';
 import React, { Suspense } from 'react';
 import AppRoutes from './AppRoutes';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
 
 const App: React.FC = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Suspense
-        fallback={
-          <div className="flex h-screen items-center justify-center">
-            <Spinner color="warning" aria-label="Loading" />
-          </div>
-        }
-      >
-        <RainbowKitProvider>
-          <AppRoutes />
-        </RainbowKitProvider>
-      </Suspense>
-    </QueryClientProvider>
+    <Suspense
+      fallback={
+        <div className="flex h-screen items-center justify-center">
+          <Spinner color="warning" aria-label="Loading" />
+        </div>
+      }
+    >
+      <AppRoutes />
+    </Suspense>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,31 @@
 import '@rainbow-me/rainbowkit/styles.css';
 import ErrorBoundary from '@src/components/common/errors/ErrorBoundary.tsx';
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import '../index.css';
 import App from './AppRoutes.tsx';
 import Layout from './components/common/layout/Layout';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { config, rainbowConfig } from './wagmi';
-import { WagmiConfig } from 'wagmi';
+import { WagmiProvider } from 'wagmi';
+import { config } from './wagmi';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <StrictMode>
     <BrowserRouter>
-      <WagmiConfig config={config}>
+      <WagmiProvider config={config}>
         <QueryClientProvider client={queryClient}>
-          <RainbowKitProvider {...rainbowConfig}>
+          <RainbowKitProvider>
             <ErrorBoundary>
               <Layout>
                 <App />
@@ -26,7 +33,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             </ErrorBoundary>
           </RainbowKitProvider>
         </QueryClientProvider>
-      </WagmiConfig>
+      </WagmiProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </StrictMode>
 );
