f77edf690cbb51566acd75a9a05a9a38d325508b
Luciferhub44 <moltenwastelands@gmail.com>
2025-01-10 11:00:48 -0500

    Refactor main.tsx and wagmi.ts to align with updated Wagmi practices
    - Replaced WagmiConfig with WagmiProvider in main.tsx for improved context management.
    - Updated wagmi.ts to simplify the configuration setup by directly using getDefaultConfig, enhancing maintainability.
    - Adjusted import paths for better consistency and clarity across the files.

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import Layout from './components/common/layout/Layout';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { config } from '@src/wagmi';
-import { WagmiConfig } from 'wagmi';
+import { WagmiProvider } from 'wagmi';
 
 const queryClient = new QueryClient();
 
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <WagmiConfig config={config}>
+        <WagmiProvider config={config}>
           <RainbowKitProvider>
             <ErrorBoundary>
               <Layout>
@@ -26,7 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
               </Layout>
             </ErrorBoundary>
           </RainbowKitProvider>
-        </WagmiConfig>
+        </WagmiProvider>
       </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,20 +1,19 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { http } from 'viem'
 import { base, mainnet } from 'wagmi/chains'
-import { createConfig } from '@wagmi/core'
+import { createConfig } from 'wagmi'
 
 // Get WalletConnect Project ID from environment variables
 const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || 'e6937fb240cd6f4df6739b75bf0b324d'
 
 // Create wagmi config
-export const config = createConfig(
-  getDefaultConfig({
-    appName: 'Tribe Odyssey',
-    projectId,
-    chains: [mainnet, base],
-    transports: {
-      [mainnet.id]: http(),
-      [base.id]: http(),
-    },
-  }),
-)
+export const config = getDefaultConfig({
+  appName: 'Tribe Odyssey',
+  projectId,
+  chains: [mainnet, base],
+  transports: {
+    [mainnet.id]: http(),
+    [base.id]: http(),
+  },
+  ssr: true // Enable if using SSR
+})
