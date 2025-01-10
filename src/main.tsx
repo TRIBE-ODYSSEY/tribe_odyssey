1a0e16bceef2b6376d4eacbf15c6c55b96f1f785
Luciferhub44 <moltenwastelands@gmail.com>
2025-01-10 12:09:55 -0500

    Refactor main.tsx to streamline provider integration and enhance context management
    - Updated main.tsx to nest WagmiConfig, QueryClientProvider, and RainbowKitProvider for improved component hierarchy.
    - Adjusted import statements for consistency and clarity, integrating rainbowConfig directly from wagmi.ts.
    - Enhanced overall structure for better maintainability and readability.

--- src/main.tsx
+++ src/main.tsx
@@ -6,28 +6,27 @@ import { BrowserRouter } from 'react-router-dom';
 import '../index.css';
 import App from './AppRoutes.tsx';
 import Layout from './components/common/layout/Layout';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { WagmiConfig } from 'wagmi'
-import { config } from './wagmi'
+import { config, rainbowConfig } from './wagmi';
+import { WagmiConfig } from 'wagmi';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <WagmiConfig config={config}>
-          <RainbowKitProvider>
+      <WagmiConfig config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider {...rainbowConfig}>
             <ErrorBoundary>
               <Layout>
                 <App />
               </Layout>
             </ErrorBoundary>
           </RainbowKitProvider>
-        </WagmiConfig>
-      </QueryClientProvider>
+        </QueryClientProvider>
+      </WagmiConfig>
     </BrowserRouter>
   </React.StrictMode>
 );
